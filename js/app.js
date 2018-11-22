600d8c5bab10bdc808334c34cb5927b7c2ade00e
cbass <sebasgp@gmail.com>
2018-11-22 12:19:55 -0300

    asdasd

---
+++ js/app.js
@@ -0,0 +1,41 @@
+'use strict'
+
+let cat = {
+    name: "pelusa",
+    age: 2,
+    hair: black,
+    purr(){
+        document.addEventListener("click",purr(){
+            function purr(alert("purr!!!!"))
+        })
+    },
+    food: {
+        favFood: "fish",
+        leastFavFood: "tomato"
+    }
+}
+
+//asd
+//those lines of codes above is an object, the elements inside are properties, it can even include a function altough it doesn't use the 
+//keywords "function",when functions are included within the curly brackets of an object, they are called "methods". Objects are cool becuse they allow
+//us organize data, so it'll look more tidy. Another neat feature is that it can contain another object with their
+// own set of properties (and even methods). cool, uh?
+
+
+//array 
+
+// Javascript treats arrays also as "object". But with is an Array? In simple words, array is a colecction of things, let's
+//
+
+let myNumbers = [2,3,4,5]
+let myColors = ["blue","red","green"]
+let myMixItems = [1,2,"blue", true]
+
+let a = myNumbers.splice(1,1);
+console.log(a);
+
+let b = myNumbers.pop();
+console.log(b);
+
+
+
